adade9a3b3c2d60ddaa7fec9f8d353ff4d862c7f
yusuf-eren <erenyusuf170@gmail.com>
2022-12-04 00:29:43 +0300

    save and get from cache

--- server/services/cache.js
+++ server/services/cache.js
@@ -3,3 +3,11 @@ import cache from 'memory-cache';
 export const clearCache = () => {
   return cache.clear();
 };
+
+export const saveToCache = async (keyword, page, movies, seconds) => {
+  return await cache.put(`${keyword}#${page}`, movies, 1000 * seconds);
+};
+
+export const getFromCache = async (keyword, page) => {
+  return await cache.get(`${keyword}#${page}`);
+};
