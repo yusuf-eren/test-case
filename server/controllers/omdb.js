3ee137eb508080ae1322eb1c931af960887f308b
yusuf-eren <erenyusuf170@gmail.com>
2022-12-03 05:15:33 +0300

    console.log() removed

--- server/controllers/omdb.js
+++ server/controllers/omdb.js
@@ -1,15 +1,11 @@
 import { searchMovies } from '../services/api/omdb.js';
 import { clearCache } from '../services/cache.js';
 
-let i = 0
-
 export const searchMovieController = async (req, res) => {
   if (!req.query.keyword) {
     return res.status(400).send({ message: 'keyword parameter is required' });
   }
 
-  console.log('triggered ', i)
-  i++
   try {
     const movies = await searchMovies(req.query.keyword, req.query.page);
     return res.status(200).send({
