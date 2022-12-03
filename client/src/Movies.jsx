b8a51669ca8300e737c41b49f2b8591c90382d39
yusuf-eren <erenyusuf170@gmail.com>
2022-12-04 00:29:35 +0300

    css modify

--- client/src/Movies.jsx
+++ client/src/Movies.jsx
@@ -1,13 +1,15 @@
 const Movies = ({ movies }) => {
   return (
-    <div>
+    <div className="row-movies">
       {movies.map((movie) => (
-        <ul style={{ listStyleType: "none" }} key={movie.imdbID}>
-          <li><img src={movie.Poster} alt={movie.Title} style={{ height: "100px", width: "100px" }} /></li>
-          <li>{movie.Title}</li>
-          <li>{movie.Year}</li>
-          <li>{movie.Type}</li>
-        </ul>
+        <div className="col-movies">
+          <ul>
+            <li className="movie-poster"><img src={movie.Poster} alt={movie.Title} /></li>
+            <li><b>{movie.Title}</b></li>
+            <li>Type: {movie.Type}</li>
+            <li>Year: {movie.Year}</li>
+          </ul>
+        </div>
       ))}
     </div>
   );
